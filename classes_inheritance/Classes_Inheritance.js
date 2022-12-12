a3d30db5ab72b8624788958fecd2f08d7221355a
Melnychuk <IMelnychuk@poland.luxoft.com>
2022-12-12 14:51:12 +0100

    feat: implement tests

--- classes_inheritance/Classes_Inheritance.js
+++ classes_inheritance/Classes_Inheritance.js
@@ -115,3 +115,25 @@ StringBuilder.prototype.get = function() {
     return this.str;
 }
 
+
+//Tests
+let intBuilder = new IntBuilder(10);
+console.log(intBuilder
+  .plus(2, 3, 2)                     // 17;
+  .minus(1, 2)                       // 14;
+  .multiply(2)                       // 28;
+  .divide(4)                         // 7;
+  .mod(3)                            // 1;
+  .get());                           // 1;
+
+console.log(IntBuilder.random(10, 100));
+
+let strBuilder = new StringBuilder('Hello');
+console.log(strBuilder
+  .plus(' all', '!')                  // 'Hello all!'
+  .minus(4)                           // 'Hello '
+  .multiply(3)                        // 'Hello Hello Hello '
+  .divide(4)                          // 'Hell';
+  .remove('l')                        // 'He';
+  .sub(1,1)                           // 'e';
+  .get());                            // -> 'e';
