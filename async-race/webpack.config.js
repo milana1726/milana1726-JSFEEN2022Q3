796fe82bb6048de6d090c91cdeae300e90003828
Melnychuk <IMelnychuk@poland.luxoft.com>
2023-01-06 01:05:06 +0100

    feat: update packages

--- async-race/webpack.config.js
+++ async-race/webpack.config.js
@@ -4,30 +4,30 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    devtool: 'inline-source-map',
+    mode: 'development',
+    devtool: 'source-map',
     entry: "./src/js/index.js",
     output: {
-        filename: "./js/index_bundle.js",
-        path: path.resolve(__dirname, "dist/")
+        filename: "index_bundle.js",
+        path: path.resolve(__dirname, "dist/"),
     },
     devServer: {
         static: {
             directory: path.join(__dirname, "dist/"),
-          },
+        },
         compress: true,
         port: 3001,
-      },
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ["@babel/preset-env"],
-                  },
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
                 },
             },
             {
@@ -38,11 +38,11 @@ module.exports = {
                 test: /\.svg}$/i,
                 use: [
                     {
-                      loader: 'url-loader',
-                      options: {
-                        limit: 10000,
-                        esModule: false
-                      },
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            esModule: false
+                        },
                     },
                 ]
             },
