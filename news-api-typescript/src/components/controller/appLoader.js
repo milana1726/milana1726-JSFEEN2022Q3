4f4367502f85197ace0ade78c2fc53eaedd5f5f8
Melnychuk <IMelnychuk@poland.luxoft.com>
2023-01-25 18:14:12 +0100

    init: start task "News API. TypeScript"

---
+++ news-api-typescript/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: '7876c797b8924d90ae70f49a8beeb6e4', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
