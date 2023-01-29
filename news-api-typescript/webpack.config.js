b5fd01ba1e218dd5d2f78ddf4283d4c8674b62ef
Melnychuk <IMelnychuk@poland.luxoft.com>
2023-01-29 23:09:05 +0100

    refactor: update webpack, eslint configs

--- news-api-typescript/webpack.config.js
+++ news-api-typescript/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
